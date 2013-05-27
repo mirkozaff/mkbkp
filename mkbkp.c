57b6cc6f8187a7ea0401ba9d73071b20933343e3
Mirko <mirko.zaff@gmail.com>
2013-05-27 17:30:09 +0200

    copia

--- mkbkp.c
+++ mkbkp.c
@@ -1,22 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "funzioni.h"
 
-int main(int argc, char** argv) {
-    
-    char name1[40];
-    char name2[40];
-    
-    strcpy(name1, argv[1]);
-    strcpy(name2, argv[2]);
-    
-    
-    
-    
-    
-    
-    
+int main(int argc, char *argv[])
+{
+  FILE *from, *to;
+  char ch;
 
-    return (EXIT_SUCCESS);
+
+  if(argc!=3) {
+    printf("Usage: copy <source> <destination>\n");
+    exit(1);
+  }
+
+  /* open source file */
+  if((from = fopen(argv[1], "rb"))==NULL) {
+    printf("Cannot open source file.\n");
+    exit(1);
+  }
+
+  /* open destination file */
+  if((to = fopen(argv[2], "wb"))==NULL) {
+    printf("Cannot open destination file.\n");
+    exit(1);
+  }
+
+  /* copy the file */
+  while(!feof(from)) {
+    ch = fgetc(from);
+    if(ferror(from)) {
+      printf("Error reading source file.\n");
+      exit(1);
+    }
+    if(!feof(from)) fputc(ch, to);
+    if(ferror(to)) {
+      printf("Error writing destination file.\n");
+      exit(1);
+    }
+  }
+
+  if(fclose(from)==EOF) {
+    printf("Error closing source file.\n");
+    exit(1);
+  }
+
+  if(fclose(to)==EOF) {
+    printf("Error closing destination file.\n");
+    exit(1);
+  }
+
+  return 0;
 }
-
