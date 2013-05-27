d14227e69c469a215a7cbaed59b21f056546c827
Mirko <mirko.zaff@gmail.com>
2013-05-27 16:30:34 +0200

    inizializzazione repo

---
+++ funzioni.h
@@ -0,0 +1,10 @@
+#ifndef FUNZIONI_H
+#define	FUNZIONI_H
+
+/**
+ * Definizione di boolean.
+ **/
+typedef enum {FALSE = 0, TRUE = 1} boolean;
+
+#endif	/* FUNZIONI_H */
+
---
+++ mkbkp.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funzioni.h"
+
+int main(int argc, char** argv) {
+    
+    char name1[40];
+    char name2[40];
+    
+    strcpy(name1, argv[1]);
+    strcpy(name2, argv[2]);
+    
+    
+    
+    
+    
+    
+    
+
+    return (EXIT_SUCCESS);
+}
+
